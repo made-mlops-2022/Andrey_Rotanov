c4e2e1185f16d4bff50308aa97a31e814ab616ef
andrey <rotanov07@mail.ru>
2022-11-01 19:11:51 +0300

    add scripts

---
+++ ml_project/models/model_fit_predict.py
@@ -0,0 +1,54 @@
+import pickle
+from typing import Dict, Tuple, Union
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, recall_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import pandas as pd
+from enities import TrainingParams
+from enities.training_params import TrainingParams
+
+SklearnClassificationModel = Union[KNeighborsClassifier, LogisticRegression]
+
+
+def train_model(features: pd.DataFrame,
+                target: pd.Series,
+                train_params: TrainingParams) -> SklearnClassificationModel:
+    if train_params.model_type == "KNeighborsClassifier":
+        model = KNeighborsClassifier(
+            n_neighbors=3
+        )
+    elif train_params.model_type == "LogisticRegression":
+        model = LogisticRegression()
+    else:
+        raise NotImplementedError()
+
+    model.fit(features, target)
+    return model
+
+
+def create_inference_pipeline(model: SklearnClassificationModel,
+                              transformer: ColumnTransformer) -> Pipeline:
+    return Pipeline([("feature_part", transformer), ("model_part", model)])
+
+
+def predict_model(model: Pipeline, features: pd.DataFrame) -> np.array:
+    return model.predict(features)
+
+
+def evaluate_model(y_pred: np.array, y_true: np.array) -> Dict[str, float]:
+    return {
+        "r2_score": r2_score(y_true, y_pred),
+        "rmse": mean_squared_error(y_true, y_pred, squared=False),
+        "mae": mean_absolute_error(y_true, y_pred),
+        'recall': recall_score(y_true, y_pred)
+    }
+
+
+def serialize_model(model: object, output_path: str) -> str:
+    with open(output_path, "wb") as f:
+        pickle.dump(model, f)
+    return output_path
